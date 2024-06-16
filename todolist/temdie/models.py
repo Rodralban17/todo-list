08c30ae3c7151104639e734467f88000267dcc8d
Temdie Alban <alban@gmail.com>
2024-06-16 19:25:27 +0200

    Temdie completed Task 1 (model-creation)

---
+++ todolist/temdie/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
+
+class TemdieTask(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.title
+    
+class TemdieTag(models.Model):
+    name = models.CharField(max_length=50)
+    task = models.ManyToManyField(TemdieTask)
+    def __str__(self):
+        return self.name
