1e2bba27a9b7d376a20746e873176b5820c468b6
Temdie Alban <alban@gmail.com>
2024-06-16 19:34:28 +0200

    Temdie completed Task 4 (register model in the admin site)

--- todolist/temdie/admin.py
+++ todolist/temdie/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, TemdieTask, TemdieTag
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(TemdieTask)
+admin.site.register(TemdieTag)
+
